8deee4dafa7c44063781d3ea67eedf78e1602a87
Joel Estrella <joelestrellan@gmail.com>
2024-04-10 14:20:07 -0400

    Created Cards base

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,65 @@
 import { useState, useEffect } from "react"
+import "./App.css"
+
+const POKEMON_DATA_API_SUFFIX = `https://pokeapi.co/api/v2/pokemon/`
 
 
 function Pokemons() {
-    const [pokemon, setPokemon] = useState(
+    const [pokemon, setPokemon] = useState([
         {
-            name: "Bulbasaur",
-            info: "Description",
-            type: "Grass"
+            name: "",
         }
+    ]
     )
+useEffect(
+    () => {
+        
+        fetch(POKEMON_DATA_API_SUFFIX)
+        .then(res => res.json())
+        .then(data => {setPokemon(data.results)})
 
-    return(
-        <div id="Compartment">
-        <h1>Pokemon</h1>
-        <h2>{pokemon.name}</h2>
-        <p>{pokemon.info}</p>
-        <p>{pokemon.type}</p>
-        </div>
+        },[])
+       
+        return(
+            <div>
+            {pokemon.map((pokemons)=> {
+                console.log(pokemons);
+             return(
+                <PokemonCard
+                name={pokemons.name}
+               
+                ></PokemonCard>
+             )
+            })}
+            </div>
+        )
+    }
 
-    )
-}
+   
+    function PokemonCard(props) {
 
+           
+            return(
+                <div id="Compartment">
+                <h1>Pokemon</h1>
+                <h2>Name:{props.name}</h2>
+                <h2>ID: {props.id}</h2>
+                {/* <h3>Weight:{pokemon.weight}</h3>
+                <p>Type 1:{(pokemon.types[0].slot)}</p> */}
+                   
+                </div>
+        
+            )
+      
+      
+    }
+    
 export function PokemonApp()
 {
-    
+
     return(
         <main>
-            <Pokemons/>
-            <Pokemons/>
+       <Pokemons/>
         </main>
     )
 }
