ec3bfc5d71117d3472d9eedd03e977110e26d837
Joel Estrella <joelestrellan@gmail.com>
2024-04-11 09:26:07 -0400

    Multiple Cards Generated by API

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,19 @@
 import { useState, useEffect } from "react"
 import "./App.css"
 
-const POKEMON_DATA_API_SUFFIX = `https://pokeapi.co/api/v2/pokemon/`
+const POKEMON_DATA_SUFFIX = `https://pokeapi.co/api/v2/pokemon?limit=20&offset=20`
+const POKEMON_DATA_API = `https://pokeapi.co/api/v2/pokemon/`
+
 
 
 function Pokemons() {
+    const [pokemonUrl, setPokemonURl] = useState([
+
+        {
+ 
+        }
+    ]
+    )
     const [pokemon, setPokemon] = useState([
         {
             name: "",
@@ -14,26 +23,66 @@ function Pokemons() {
 useEffect(
     () => {
         
-        fetch(POKEMON_DATA_API_SUFFIX)
+        fetch(POKEMON_DATA_SUFFIX)
         .then(res => res.json())
         .then(data => {setPokemon(data.results)})
+      
 
+     
         },[])
+       useEffect(() =>
+       {
+       pokemon.map((pokemons)=> {
+        const POKEMON_ARRAY = POKEMON_DATA_API+pokemons.name
+   
+        fetch(POKEMON_ARRAY) .then(res => res.json() ) .then(data => {setPokemonURl(oldData =>[...oldData, data])})
        
+       })
+   
+
+       
+    },[pokemon])
+
+   
         return(
-            <div>
-            {pokemon.map((pokemons)=> {
-                console.log(pokemons);
-             return(
-                <PokemonCard
-                name={pokemons.name}
-               
-                ></PokemonCard>
-             )
-            })}
+            <div id="Mapper">
+            { 
+                
+                pokemonUrl.map((pokemonData) => {
+                   
+                    if(pokemonData.name != null)
+                    {
+                        console.log(pokemonData)
+                    return(
+                    <PokemonCard
+                    image={pokemonData.sprites.front_default}
+                    name={pokemonData.name}
+                    weight={pokemonData.weight}
+                    key={pokemonData.id}
+                    id={pokemonData.id}
+                    ></PokemonCard>
+                    )
+                }
+                })
+            
+                   
+                
+                 
+            
+        
+    
+                }
+                
+
             </div>
         )
     }
+
+            
+        
+    
+    
+    
 
    
     function PokemonCard(props) {
@@ -41,8 +90,10 @@ useEffect(
            
             return(
                 <div id="Compartment">
-                <h1>Pokemon</h1>
+                <h1 text-content="center">Pokemon</h1>
+                <img id="PokeIMG" src={props.image}></img>
                 <h2>Name:{props.name}</h2>
+                <h2>Weight:{props.weight}</h2>
                 <h2>ID: {props.id}</h2>
                 {/* <h3>Weight:{pokemon.weight}</h3>
                 <p>Type 1:{(pokemon.types[0].slot)}</p> */}
